31b8441c014dd3db9ad500c2b1e26e4b592d44d0
akhilajoshi05 <163162577+akhilajoshi05@users.noreply.github.com>
2024-08-01 12:56:24 +0530

    Update urls.py

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,20 +1,10 @@
+
 from django.contrib import admin
 from django.urls import path
-from . import views
-
+from .views import add_item, get_all_items, update_item, delete_item
 urlpatterns = [
-    path('',views.index ),
-    # path('add/',views.add_person ),
-    path('add/',views.add_item ),
-    # path('show/',views.get_all_persons ),
-    path('show/',views.get_all_items ),
+    path('add/', add_item, name='add_item'),
+    path('show/', get_all_items, name='get_all_items'),
+    path('update/', update_item, name='update_item'),
+    path('delete/', delete_item, name='delete_item'),
 ]
-
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('', views.index, name='index'),
-#     path('add_person/', views.add_person, name='add_person'),
-#     path('get_all_persons/', views.get_all_persons, name='get_all_persons'),
-# ]
